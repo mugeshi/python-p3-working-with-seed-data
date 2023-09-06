38cb552b37d4a04ee7f3c0cda470a6714bb3607c
mugeshi <nicolemugeshi@gmail.com>
2023-09-06 05:58:13 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -10,7 +10,29 @@ from models import Game
 fake = Faker()
 
 if __name__ == '__main__':
+
     
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+
+    # clear old data
+    session.query(Game).delete()
+    session.commit()
+
+
+# Add a console message so we can see output when the seed file runs
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.add_all(games)
+session.commit()
